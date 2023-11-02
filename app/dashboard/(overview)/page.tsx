96d9b6b0e27ee373ebe1cec5095a8f69569caeb0
Jonathan Varela <jonvarela4@gmail.com>
2023-11-02 12:32:58 -0400

    first commit

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Page() {
+  return (
+    <main>
+      <h1>Dashboard hello jonathan </h1>
+      <div></div>
+      <div></div>
+    </main>
+  );
+}
+
+export default Page;
---
+++ app/dashboard/invoices/page.ts
@@ -0,0 +1,1 @@
+// import Pagination from '@/app/ui/invoices/pagination';
