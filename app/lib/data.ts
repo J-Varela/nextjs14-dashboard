8e5dd2f56d3b1a3bbed47656e34bb970619e0f54
Jonathan Varela <jonvarela4@gmail.com>
2023-11-07 20:05:09 -0500

    minor bugs

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -20,10 +20,12 @@ export async function fetchRevenue() {
     // Dont't do this in real life :)
 
     // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000 ))
+    // await new Promise((resolve) => setTimeout(resolve, 3000 ));
+
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
     //console.log('Data fetch complete after 3 seconds.)
+
     return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
@@ -39,8 +41,7 @@ export async function fetchLatestInvoices() {
       FROM invoices
       JOING customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
-      LIMIT 5
-    `;
+      LIMIT 5`;
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
@@ -118,6 +119,7 @@ export async function fetchFilteredInvoices(
       ORDER BY invoices.data DESC 
       LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
     `;
+
     return invoices.rows;
   } catch (error) {
     console.error('Database Error:', error);
@@ -135,12 +137,14 @@ export async function fetchInvoicesPage(query: string) {
       customers.name ILIKE ${`%${query}%`} OR
       customers.email ILIKE ${`%${query}%`} OR
       invoices.amount::text ILIKE ${`%${query}%`} OR
+      invoices.date::text ILIKE ${`%${query}%`} OR
       invoices.status ILIKE ${`%${query}%`}
     `;
 
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
     return totalPages;
   } catch (error) {
+    console.error('Databse Error:', error);
     throw new Error('Failed to fetch total number of invoices.');
   }
 }
@@ -149,12 +153,13 @@ export async function fetchInvoiceById(id: string) {
   noStore();
   try {
     const data = await sql<InvoiceForm>`
-      invoices.id,
-      invoices.customer_id,
-      invoices.amount,
-      invoices.status
-    FROM invoices
-    WHERE invoices.id = ${id};
+      SELECT
+        invoices.id,
+        invoices.customer_id,
+        invoices.amount,
+        invoices.status
+      FROM invoices
+      WHERE invoices.id = ${id};
     `;
 
     const invoice = data.rows.map((invoice) => ({
@@ -179,10 +184,55 @@ export async function fetchCustomers() {
     FROM customers
     ORDER BY name ASC
     `;
+
     const customers = data.rows;
     return customers;
   } catch (err) {
     console.error('Database Error:', err);
     throw new Error('Failed to fetch all customers');
+  }
+}
+
+export async function fetchFilteredCustomers(query: string) {
+  noStore();
+  try {
+    const data = await sql<CustomersTable>`
+		SELECT
+		  customers.id,
+		  customers.name,
+		  customers.email,
+		  customers.image_url,
+		  COUNT(invoices.id) AS total_invoices,
+		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
+		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
+		FROM customers
+		LEFT JOIN invoices ON customers.id = invoices.customer_id
+		WHERE
+		  customers.name ILIKE ${`%${query}%`} OR
+        customers.email ILIKE ${`%${query}%`}
+		GROUP BY customers.id, customers.name, customers.email, customers.image_url
+		ORDER BY customers.name ASC
+	  `;
+
+    const customers = data.rows.map((customer) => ({
+      ...customer,
+      total_pending: formatCurrency(customer.total_pending),
+      total_paid: formatCurrency(customer.total_paid),
+    }));
+
+    return customers;
+  } catch (err) {
+    console.error('Database Error:', err);
+    throw new Error('Failed to fetch customer table.');
+  }
+}
+
+export async function getUser(email: string) {
+  try {
+    const user = await sql`SELECT * from USERS where email=${email}`;
+    return user.rows[0] as User;
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
   }
 }
